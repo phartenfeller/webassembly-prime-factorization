b742a6801a1c30624e20b151b22ef0e8fd193bea
phartenfeller <pharti@hotmail.de>
2021-04-12 22:41:59 +0200

    first implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,62 @@
 <script>
+  const IDLE = 0;
+  const PROCESSING = 1;
+  const FINISHED = 2;
+
+  const worker = new Worker('/primeFactorizationWorker.js');
+
+  let state = IDLE;
+  let input = 100;
+  let result = null;
+  let showInput = null;
+  let startTime;
+  let seconds = null;
+
+  const handleClick = () => {
+    state = PROCESSING;
+    result = null;
+    showInput = input;
+    startTime = new Date().getTime();
+    worker.postMessage(input);
+  };
+
+  worker.addEventListener(
+    'message',
+    (e) => {
+      let endTime = new Date().getTime();
+      state = FINISHED;
+      result = e.data;
+      seconds = ((endTime - startTime) / 1000).toFixed(4);
+    },
+    false
+  );
 </script>
 
 <main class="mx-16 mt-6">
-	<h1 class="text-4xl text-indigo-500">Svelte + TailwindCSS Template</h1>
-	<p>Visit the <a class="text-indigo-700 hover:underline" href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1 class="text-4xl text-indigo-500 mb-4">Prime Factorization</h1>
+  <input
+    bind:value={input}
+    type="number"
+    class="bg-gray-100 rounded p-2 mr-3 focus:ring focus:ring-indigo-400 focus:outline-none"
+  />
+  <button
+    disabled={state === PROCESSING}
+    type="button"
+    class="bg-indigo-500 rounded px-3 py-2 text-gray-100 hover:bg-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 disabled:bg-gray-400 disabled:cursor-not-allowed"
+    on:click={handleClick}>Calc</button
+  >
+  <div class="mt-4 text-lg">
+    {#if state === FINISHED}
+      <p class="font-semibold ">
+        Result for {showInput} is: {result.join(', ')}.
+      </p>
+      <p>Calculation took {seconds} seconds.</p>
+    {:else if state === PROCESSING}
+      <p>Calculating...</p>
+    {:else if state === IDLE}
+      <p>Press button to start calc</p>
+    {/if}
+  </div>
 </main>
 
 <style global>

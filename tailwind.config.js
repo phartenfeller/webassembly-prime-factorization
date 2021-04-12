208aabeb5d26309cf8a967b4475b6c6feb201447
Philipp Hartenfeller <34486873+phartenfeller@users.noreply.github.com>
2021-04-12 20:51:20 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: ['./public/index.html', './src/**/*.svelte'],
+    options: {
+      defaultExtractor: (content) => [
+        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+        ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
+      ],
+    },
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
